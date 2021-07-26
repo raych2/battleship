a0e83c942848c95389f34e3fa22e687d3f31f180
raych2 <rtongco@gmail.com>
2021-07-25 21:36:17 -0700

    Add plugins to fix problem with build during production mode

--- build-utils/webpack.common.js
+++ build-utils/webpack.common.js
@@ -1,7 +1,29 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Battleship',
+      template: path.resolve(__dirname, '..', './src/template.html'),
+      filename: 'index.html',
+    })
+  ],
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, '..', 'dist'),
